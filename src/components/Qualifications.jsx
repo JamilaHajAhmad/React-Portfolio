19ed636a191b97d966ab18fd11ab9110908be22a
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-01 11:03:44 +0200

    add Qualifications component to display educational background

---
+++ src/components/Qualifications.jsx
@@ -0,0 +1,25 @@
+function Qualifications() {
+    return(
+        <div>
+            <h2>Qualifications</h2>
+            <ul>
+                <li>
+                    <h3>Bachelor Degree in Computer Systems Engineering </h3>
+                    <a href="https://ptuk.edu.ps/ar/" target="_blank"><p>Palestine Technical University - Kadoorie</p></a>
+                </li>
+                <hr/>
+                <li>
+                    <h3>Front-End Developer Nanodegree</h3>
+                    <a href="https://www.udacity.com" target="_blank"><p>Udacity</p></a>
+                </li>
+                <hr/>
+                <li>
+                    <h3>Data Analyst Nanodegree</h3>
+                    <a href="https://www.udacity.com" target="_blank"><p>Udacity</p></a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Qualifications;
