01f986db0adec93ee2063237a4396247d4fd4959
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-03 16:40:05 +0200

    add social media data module with links and images

---
+++ src/data/socials.js
@@ -0,0 +1,26 @@
+import linkedin from '../assets/linkedin.jpg';
+import github from '../assets/github.jpg';
+import instagram from '../assets/instagram.jpg';
+
+const SOCIALS = [
+    {
+        id: 1,
+        name: 'LinkedIn',
+        img: linkedin,
+        link: 'https://www.linkedin.com'
+    },
+    {
+        id: 2,
+        name: 'GitHub',
+        img: github,
+        link: 'https://www.github.com'
+    },
+    {
+        id: 3,
+        name: 'Instagram',
+        img: instagram,
+        link: 'https://www.instagram.com'
+    }
+]
+
+export default SOCIALS;
