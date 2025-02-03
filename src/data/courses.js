33ab1d17aea47abf5d827b847efa02348dea55dd
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-03 08:44:22 +0200

    add courses data module with initial course information and images

---
+++ src/data/courses.js
@@ -0,0 +1,29 @@
+import course1 from '../assets/course1.jpg';
+import course2 from '../assets/course2.jpg';
+import course3 from '../assets/course3.jpg';
+
+const COURSES = [
+    {
+        id: 1,
+        title: 'HTML',
+        description: 'Learn the basics of HTML, the essential language for creating web pages',
+        img: course1,
+        link: 'https://www.w3schools.com/html/'
+    },
+    {
+        id: 2,
+        title: 'CSS',
+        description: 'Learn the basics of CSS, a stylesheet language used to design the look of a web page',
+        img: course2,
+        link: 'https://www.w3schools.com/css/'
+    },
+    {
+        id: 3,
+        title: 'JavaScript',
+        description: 'Learn the basics of JavaScript, a programming language that enables you to interact with web pages',
+        img: course3,
+        link: 'https://www.w3schools.com/js/'
+    }
+]
+
+export default COURSES;
