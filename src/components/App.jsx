11810e0a015c95e8c324335c499b67d4a50b9e1c
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-03 07:00:40 +0200

    add Courses component to App layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import Profile from "./Profile"
 import Qualifications from "./Qualifications";
+import Courses from "./Courses";
 import "../main.css"
 
 function App() {
@@ -11,6 +12,12 @@ function App() {
                 </div>
                 <div>
                     <Qualifications />
+                </div>
+            </div>
+            <hr></hr>
+            <div className="row">
+                <div>
+                <Courses />
                 </div>
             </div>
         </div>
