fcd52cb13f270b39244625bd8ce4245a26cd4a67
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-04 10:45:14 +0200

    add: integrate Title component into App for improved page structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,26 @@ import Qualifications from "./Qualifications";
 import Courses from "./Courses";
 import "../main.css"
 import Socials from "./Socials";
+import Title from "./Title";
 
 function App() {
     return (
-        <div className="container">
-            <div className="row">
-                <div>
-                    <Profile />
+        <div>
+            <Title />
+            <div className="container">
+                <div className="row">
+                    <div>
+                        <Profile />
+                    </div>
+                    <div>
+                        <Qualifications />
+                    </div>
                 </div>
                 <div>
-                    <Qualifications />
+                    <Courses />
                 </div>
+                <Socials />
             </div>
-            <div>
-                <Courses />
-            </div>
-            <Socials />
         </div>
     )
 }
