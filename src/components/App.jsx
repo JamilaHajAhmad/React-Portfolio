eefe96877059d4e8377bf3dd2830a850544cbbd4
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-07 10:48:43 +0200

    add: include Header component in App for improved layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import Courses from "./Courses";
 import "../main.css"
 import Socials from "./Socials";
 import Title from "./Title";
+import Header from "./Header";
 
 function App() {
     return (
         <div>
+            <Header />
             <Title />
             <div className="container">
                 <div className="row">
