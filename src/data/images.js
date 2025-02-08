c6e90a18b6fb8d813a51623a8268f699238d8943
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-08 19:24:24 +0200

    add: create images.js to define a collection of image assets with descriptions

---
+++ src/data/images.js
@@ -0,0 +1,60 @@
+import alexandria from '../assets/alexandria.jpg';
+import beirut from '../assets/beirut.jpg';
+import giza from '../assets/giza.jpg';
+import london from '../assets/london.jpg';
+import paris from '../assets/paris.jpg';
+import petra from '../assets/petra.jpg';
+import rome from '../assets/rome.jpg';
+import seoul from '../assets/seoul.jpg';
+const IMAGES = [
+    {
+        id: 1,
+        name: 'Alexandria',
+        src: alexandria,
+        description: 'The ancient city of Alexandria, Egypt'
+    },
+    {
+        id: 2,
+        name: 'Beirut',
+        src: beirut,
+        description: 'The capital of Lebanon'
+    },
+    {
+        id: 3,
+        name: 'Giza',
+        src: giza,
+        description: 'The Giza pyramid complex, Egypt'
+    },
+    {
+        id: 4,
+        name: 'London',
+        src: london,
+        description: 'The capital of England'
+    },
+    {
+        id: 5,
+        name: 'Paris',
+        src: paris,
+        description: 'The capital of France'
+    },
+    {
+        id: 6,
+        name: 'Petra',
+        src: petra,
+        description: 'The ancient city of Petra, Jordan'
+    },
+    {
+        id: 7,
+        name: 'Rome',
+        src: rome,
+        description: 'The capital of Italy'
+    },
+    {
+        id: 8,
+        name: 'Seoul',
+        src: seoul,
+        description: 'The capital of South Korea'
+    }
+];
+
+export default IMAGES;
