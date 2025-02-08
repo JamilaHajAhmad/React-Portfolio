a7435cde915fb5ebc86dfe3663972fd043aaf4f5
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-02-08 18:32:33 +0200

    add: implement Quotes component to fetch and display random quotes

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+
 function Quotes() {
-    return <h2>Today&apos;s Quote</h2>
+    const [quote, setQuote] = useState({});
+    const [quotes, setQuotes] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3007/quote").
+        then(response => response.json()).
+        then(data => setQuote(data));
+    }, []);
+
+    const fetchQuotes = () => {
+        fetch("http://localhost:3007/quotes").
+        then(response => response.json()).
+        then(data => setQuotes(data));
+    }
+    return (
+        <div>
+            <h2>Today&apos;s Quote</h2>
+            <p>{quote.content}</p>
+            <hr />
+            <h2>Do you wanna more?</h2>
+            <button onClick={()=> fetchQuotes()}>Show More</button>
+            {
+                quotes.map(quote => (
+                    <p key={quote.id}>{quote.content}</p>
+                ))
+            }
+        </div>
+    )
 }
 
 export default Quotes;
